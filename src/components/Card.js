7010d8d6951db1ffbe28a3bb7160a027c83ce711
Elwoode <elwoode@yandex.ru>
2022-04-06 12:05:55 +0300

    спринт 10 #2

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,19 @@
 import React from "react";
 
-function Card(props) {
+function Card({ onCardClick, card, link, name, likes }) {
   function handleClick() {
-    props.onCardClick(props.card)
+    onCardClick(card)
   }
 
   return (
     <div className="elem">
-      <img className="elem__image" src={props.link} alt={props.name} title="Изображение локации указанной в описаннии к фотографии" onClick={handleClick} />
+      <img className="elem__image" src={link} alt={name} title="Изображение локации указанной в описаннии к фотографии" onClick={handleClick} />
       <button type="button" className="elem__del-button" title="Удалить" />
       <div className="elem__info">
-        <h2 className="elem__text">{props.name}</h2>
+        <h2 className="elem__text">{name}</h2>
         <div className="elem__like-container">
           <button type="button" className="elem__like-button" title="Нравится" />
-          <p className="elem__like-counter">{props.likes}</p>
+          <p className="elem__like-counter">{likes}</p>
         </div>
       </div>
     </div>
